cefdd88ccf81b25e9b2a1b8d907c7cff607e390b
Alejandro JNM <alejandrojnm@gmail.com>
2019-07-21 02:01:17 -0400

    Add snapshots, volumes class to civo

--- civo.py
+++ civo.py
@@ -25,6 +25,9 @@ class Civo:
         # Ssh class
         self.ssh = self.Ssh(self.headers)
         self.instances = self.Instances(self.headers)
+        self.networks = self.Networks(self.headers)
+        self.snapshots = self.Snapshots(self.headers)
+        self.volumes = self.Volumes(self.headers)
 
     class Ssh:
         """
@@ -332,6 +335,153 @@ class Civo:
         def delete(self, id: str) -> object:
             """
             Function to removing a private network
+            :return: object json
+            """
+            r = requests.delete(self.url + '/{}'.format(id), headers=self.headers)
+
+            return r.json()
+
+    class Snapshots:
+        """
+        We provide a backup service for our Instances called snapshots. This takes an exact copy of the instance's
+        virtual hard drive. At any point an instance can be restored to the state it was in when the snapshot was made.
+        These snapshots can also be used to build a new instance to scale identically configured infrastructure.
+
+        As snapshot storage is chargeable, at any time these can be deleted. They can also be scheduled rather than
+        immediately created, and if desired repeated at the same schedule each week (although the repeated snapshot
+        will overwrite itself each week not keep multiple weekly snapshots).
+        """
+
+        def __init__(self, headers):
+            self.headers = headers
+            self.url = 'https://api.civo.com/v2/snapshots'
+
+        def create(self, name: str, instance_id: str, safe: str = 'false', cron_timing: str = None) -> object:
+            """
+            Function to create a new or update an old snapshot
+            :param instance_id: The ID of the instance to snapshot
+            :param name: The name of the instance
+            :param safe: If true the instance will be shut down during the snapshot to ensure all files are in a
+                         consistent state (e.g. database tables aren't in the middle of being optimised and hence
+                         risking corruption). The default is false so you experience no interruption of service,
+                         but a small risk of corruption.
+            :param cron_timing: If a valid cron string is passed, the snapshot will be saved as an automated snapshot,
+                                continuing to automatically update based on the schedule of the cron sequence provided.
+                                The default is nil meaning the snapshot will be saved as a one-off snapshot.
+            :return: objects json
+            """
+            payload = {'instance_id': instance_id}
+
+            if safe:
+                payload['safe'] = safe
+
+            if cron_timing:
+                payload['cron_timing'] = cron_timing
+
+            r = requests.put(self.url + '/{}'.format(name), headers=self.headers, params=payload)
+
+            return r.json()
+
+        def list(self) -> object:
+            """
+            Function to list snapshots
+            :return: object json
+            """
+            r = requests.get(self.url, headers=self.headers)
+
+            return r.json()
+
+        def delete(self, name: str) -> object:
+            """
+            Function to deleting a snapshot
+            :param name: name of the instance
+            :return: object json
+            """
+            r = requests.delete(self.url + '/{}'.format(name), headers=self.headers)
+
+            return r.json()
+
+    class Volumes:
+        """
+        We provide a flexible size additional storage service for our Instances called volumes.
+        This creates and attaches an additional virtual disk to the instance, allowing you to put backups or database
+        files on the separate volume and later move the volume to another instance.
+
+        As volume storage is chargeable, at any time these can be deleted.
+        """
+
+        def __init__(self, headers):
+            self.headers = headers
+            self.url = 'https://api.civo.com/v2/volumes'
+
+        def create(self, name: str, size_gb: str, bootable: str = 'false') -> object:
+            """
+            Function to create a new volume
+            :param name: A name that you wish to use to refer to this volume (required)
+            :param size_gb: A minimum of 1 and a maximum of your available disk space from your quota specifies
+                            the size of the volume in gigabytes (required).
+            :param bootable: Mark the volume as bootable with a boolean (optional; defaults to false).
+            :return: objects json
+            """
+            payload = {'name': name, 'size_gb': size_gb}
+
+            if bootable:
+                payload['bootable'] = bootable
+
+            r = requests.put(self.url + '/{}'.format(name), headers=self.headers, params=payload)
+
+            return r.json()
+
+        def list(self) -> object:
+            """
+            Function to list volumes
+            :return: object json
+            """
+            r = requests.get(self.url, headers=self.headers)
+
+            return r.json()
+
+        def resizing(self, id: str, size_gb: str) -> object:
+            """
+            Function to resizing a volume
+            :param id: id of the objects
+            :param size_gb: A minimum of the existing size of the volume plus 1 and a maximum of your available
+                            disk space from your quota specifies the size of the volume in gigabytes (required).
+            :return: object json
+            """
+            payload = {'size_gb': size_gb}
+
+            r = requests.put(self.url + '/{}/resize'.format(id), headers=self.headers, params=payload)
+
+            return r.json()
+
+        def attach(self, id: str, instance_id: str) -> object:
+            """
+            Function to attach a volume to an instance
+            :param id: id of the objects
+            :param instance_id: The ID of an instance that you wish to attach this volume to (required)
+            :return: object json
+            """
+            payload = {'instance_id': instance_id}
+
+            r = requests.put(self.url + '/{}/attach'.format(id), headers=self.headers, params=payload)
+
+            return r.json()
+
+        def attach(self, id: str) -> object:
+            """
+            Function to detach a volume from an instance
+            :param id: id of the objects
+            :return: object json
+            """
+            r = requests.put(self.url + '/{}/detach'.format(id), headers=self.headers)
+
+            return r.json()
+
+        def delete(self, id: str) -> object:
+            """
+            Function to deleting a volume
+            :param id: name of the instance
             :return: object json
             """
             r = requests.delete(self.url + '/{}'.format(id), headers=self.headers)
