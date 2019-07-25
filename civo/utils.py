2ddee268654c1aead730dc97db48f1a2003d0d3f
Alejandro JNM <alejandrojnm@gmail.com>
2019-07-24 23:45:36 -0400

    Fix the filter in utils.py

--- civo/utils.py
+++ civo/utils.py
@@ -8,10 +8,15 @@ def filter_list(data, filter):
     Function to handle filter in all list of the api
     :param data: object json
     :param searchs: the filter in this format (label:Home, id:6224cd2b-d416-4e92-bdbb-db60521c8eb9)
-    :return: the found objects in json format
+    :return: the found object in json format
     """
     filter_split = filter.split(':')
     search_in = filter_split[0]
     search = filter_split[1]
 
-    return [element for element in data if search in element[search_in]]
+    try:
+        search = int(search)
+    except ValueError:
+        pass
+
+    return [element for element in data if element[search_in] == search]
